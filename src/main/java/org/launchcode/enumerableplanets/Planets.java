08582dccbb0d84a87cdb43c54a03e3ce2a7699d8
EyeTyrant <1414jg@gmail.com>
2019-12-29 13:40:42 -0600

    myPlanets1 branch 14.3 Exercise: Enum Practice step 11 complete

--- src/main/java/org/launchcode/enumerableplanets/Planets.java
+++ src/main/java/org/launchcode/enumerableplanets/Planets.java
@@ -2,24 +2,29 @@ package org.launchcode.enumerableplanets;
 
 public enum Planets {
 
-  MERCURY("Mercury"),
-  VENUS("Venus"),
-  EARTH("Earth"),
-  MARS("Mars"),
-  JUPITER("Jupiter"),
-  SATURN("Saturn"),
-  URANUS("Uranus"),
-  NEPTUNE("Neptune");
+  MERCURY("Mercury", 88),
+  VENUS("Venus", 225),
+  EARTH("Earth", 365),
+  MARS("Mars", 687),
+  JUPITER("Jupiter", 4333),
+  SATURN("Saturn", 10759),
+  URANUS("Uranus", 30687),
+  NEPTUNE("Neptune", 60200);
 
 private final String planetName;
+private final int yearLength;
 
-
-  Planets(String planetName) {
+  Planets(String planetName, int yearLength) {
     this.planetName = planetName;
+    this.yearLength = yearLength;
   }
 
 
   public String getPlanetName() {
     return planetName;
+  }
+
+  public int getYearLength() {
+    return yearLength;
   }
 }
